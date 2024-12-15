098bd800b55894342aa3b96bf221a154717b6de8
Vladislav <talcore@yandex.ru>
2024-12-15 13:43:31 +0500

    add: parsers

---
+++ internal/parsers/parser_hb.go
@@ -0,0 +1,84 @@
+package parsers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html/charset"
+)
+
+type RequestOpts struct {
+	Method  string
+	Head    map[string]string
+	Cookies map[string]string
+	Proxy   map[string]string
+}
+
+type ParserHB struct {
+	UrlHB string
+}
+
+func (hb *ParserHB) GetHtmlPage(reqOptions *RequestOpts) (*goquery.Document, error) {
+
+	if reqOptions.Method == "" {
+		return nil, fmt.Errorf("no method name, impossible to make")
+	}
+
+	if hb.UrlHB == "" {
+		return nil, fmt.Errorf("no url, impossible to make")
+	}
+
+	// Создаем новый http запрос
+	client := &http.Client{}
+
+	request, err := http.NewRequest(reqOptions.Method, hb.UrlHB, nil)
+	if err != nil {
+		fmt.Println("Ошибка во время исполнения запрос", err)
+	}
+
+	if reqOptions.Head != nil {
+		key, ok := reqOptions.Head["Host"]
+		if !ok {
+			key, ok := reqOptions.Head["host"]
+			if ok {
+				request.Header.Set(key, reqOptions.Head["host"])
+			}
+
+		} else {
+			request.Header.Set(key, reqOptions.Head["Host"])
+		}
+
+		for key, value := range reqOptions.Head {
+			request.Header.Set(key, value)
+		}
+	}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		fmt.Println("Ошибка во время исполнения запроса", err)
+	}
+
+	fmt.Println(response.StatusCode)
+
+	utf8, err := charset.NewReader(response.Body, response.Header.Get("Content-Type"))
+	if err != nil {
+		println("Ошибка charset", err)
+		return nil, err
+	}
+
+	document, err := goquery.NewDocumentFromReader(utf8)
+	if err != nil {
+		println("Ошибка парсинга документа из UTF-8", err)
+	}
+
+	defer response.Body.Close()
+
+	return document, nil
+
+}
+
+func (hb *ParserHB) GetUrlsArticles() {
+
+}
