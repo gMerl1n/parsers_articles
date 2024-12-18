6401eac306bf0344fc26c1a86c10cac6445e8d65
Vladislav <talcore@yandex.ru>
2024-12-18 13:20:21 +0500

    add: services for cat, articles, parsers

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,33 +1,15 @@
 package service
 
-import (
-	"context"
+import "github.com/gMerl1n/parsers_articles/internal/repository"
 
-	"github.com/gMerl1n/parsers_articles/internal/repository"
-)
-
-type Services interface {
-	CreateCategory(ctx context.Context, providerSign string, url string) (int, error)
-	GetCategory(ctx context.Context) ([]string, error)
+type Services struct {
+	ServiceArticles ServiceArticles
+	ServiceCategory ServiceCategory
 }
 
-type service struct {
-	storage repository.Storage
-}
-
-func NewServices(repo *repository.Repository) Services {
-	return &service{storage: repo}
-}
-
-func (s *service) CreateCategory(ctx context.Context, providerSign string, url string) (int, error) {
-	categoryID, err := s.storage.CreateCategory(ctx, url, providerSign)
-	if err != nil {
-		return 0, err
+func NewServices(repo *repository.Repositories) *Services {
+	return &Services{
+		ServiceArticles: NewArticlesSerivce(repo.Articles),
+		ServiceCategory: NewCategoryService(repo.Categories),
 	}
-
-	return categoryID, nil
-}
-
-func (s *service) GetCategory(ctx context.Context) ([]string, error) {
-	return nil, nil
 }
