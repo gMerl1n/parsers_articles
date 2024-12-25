c60ab962105ef912de4d3e9c0b530daf93f28dd2
Vladislav <talcore@yandex.ru>
2024-12-25 14:31:42 +0500

    add: articles services

---
+++ 01_searcher_articles/internal/services/articles_service.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"context"
+
+	"github.com/gMerl1on/parsers_articles/01_searcher_articles/internal/entities"
+	"github.com/gMerl1on/parsers_articles/01_searcher_articles/internal/repository"
+	"go.uber.org/zap"
+)
+
+type ArticleService struct {
+	repo   repository.RepoArticle
+	logger *zap.Logger
+}
+
+func NewArticlesSerivce(repo repository.RepoArticle, log *zap.Logger) *ArticleService {
+	return &ArticleService{
+		repo:   repo,
+		logger: log,
+	}
+}
+
+type ServiceArticle interface {
+	CreateArticles(ctx context.Context, articles []entities.Article) (bool, error)
+}
+
+func (a *ArticleService) CreateArticles(ctx context.Context, articles []entities.Article) (bool, error) {
+	res, err := a.repo.CreateArticles(ctx, articles)
+	if err != nil {
+		return false, err
+	}
+
+	return res, err
+
+}
--- 01_searcher_articles/internal/services/services.go
+++ 01_searcher_articles/internal/services/services.go
@@ -1,1 +1,16 @@
 package services
+
+import (
+	"github.com/gMerl1on/parsers_articles/01_searcher_articles/internal/repository"
+	"go.uber.org/zap"
+)
+
+type Services struct {
+	ServiceArticle ServiceArticle
+}
+
+func NewServices(repo repository.Repositories, log *zap.Logger) *Services {
+	return &Services{
+		ServiceArticle: NewArticlesSerivce(repo.Articles, log),
+	}
+}
