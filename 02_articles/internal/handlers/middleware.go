bddca40dc2d02a53ef577c904515765a918fe3f6
Vladislav <talcore@yandex.ru>
2025-01-09 16:25:22 +0500

    fix: middleware

--- 02_articles/internal/handlers/middleware.go
+++ 02_articles/internal/handlers/middleware.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gMerl1on/parsers_articles/02_articles/constants"
 )
@@ -24,6 +25,39 @@ func SortProviderSignMiddleware(h http.HandlerFunc) http.HandlerFunc {
 
 		options := SortProviderSignOption{
 			providerSign: providerSign,
+		}
+
+		ctx := context.WithValue(request.Context(), constants.OptionsContextKey, options)
+		request = request.WithContext(ctx)
+
+		h(response, request)
+	}
+}
+
+type CategoryIDOption struct {
+	ID int
+}
+
+func CategoryIDMiddleware(h http.HandlerFunc) http.HandlerFunc {
+	return func(response http.ResponseWriter, request *http.Request) {
+
+		categoryID := request.URL.Query().Get("category_id")
+
+		if categoryID == "" {
+			response.WriteHeader(http.StatusBadRequest)
+			response.Write([]byte("Нельзя так делать: не передан id категории"))
+			return
+		}
+
+		categoryIDInt, err := strconv.Atoi(categoryID)
+		if err != nil {
+			response.WriteHeader(http.StatusBadRequest)
+			response.Write([]byte("Не получилось перевести id категории в int"))
+			return
+		}
+
+		options := CategoryIDOption{
+			ID: categoryIDInt,
 		}
 
 		ctx := context.WithValue(request.Context(), constants.OptionsContextKey, options)
