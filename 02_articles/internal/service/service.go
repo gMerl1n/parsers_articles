eec392e9163dddd895e3d0af4d60608afb924418
Vladislav <talcore@yandex.ru>
2025-01-12 23:13:46 +0500

    add: redis in user services

--- 02_articles/internal/service/service.go
+++ 02_articles/internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/gMerl1on/parsers_articles/02_articles/internal/repository"
+	"github.com/gMerl1on/parsers_articles/02_articles/pkg/jwt"
 	"go.uber.org/zap"
 )
 
@@ -9,12 +10,13 @@ type Services struct {
 	ServiceArticles ServiceArticles
 	ServiceCategory ServiceCategory
 	ServiceUser     ServiceUser
+	TokenManager    jwt.TokenManager
 }
 
-func NewServices(repo *repository.Repositories, logger *zap.Logger) *Services {
+func NewServices(repo *repository.Repositories, logger *zap.Logger, TokenManager jwt.TokenManager) *Services {
 	return &Services{
 		ServiceArticles: NewArticlesSerivce(repo.Articles, logger),
 		ServiceCategory: NewCategoryService(repo.Categories, logger),
-		ServiceUser:     NewUserService(repo.Users, logger),
+		ServiceUser:     NewUserService(repo.Users, logger, TokenManager, repo.UserRedis),
 	}
 }
--- 02_articles/internal/service/user_service.go
+++ 02_articles/internal/service/user_service.go
@@ -3,26 +3,33 @@ package service
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/gMerl1on/parsers_articles/02_articles/internal/domain"
 	"github.com/gMerl1on/parsers_articles/02_articles/internal/repository"
 	er "github.com/gMerl1on/parsers_articles/02_articles/pkg/errors"
+	"github.com/gMerl1on/parsers_articles/02_articles/pkg/jwt"
 	"go.uber.org/zap"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
-	repo   repository.StorageUser
-	logger *zap.Logger
+	repo         repository.StorageUser
+	logger       *zap.Logger
+	tokenManager jwt.TokenManager
+	redisUser    repository.RedisStorageUser
 }
 
 type ServiceUser interface {
 	CreateUser(ctx context.Context, name, surname, email, password, repeatPassword string, age int) (int, error)
 }
 
-func NewUserService(repo repository.StorageUser, log *zap.Logger) *UserService {
+func NewUserService(repo repository.StorageUser, log *zap.Logger, tokenManager jwt.TokenManager, redisUser repository.RedisStorageUser) *UserService {
 	return &UserService{
-		repo:   repo,
-		logger: log,
+		repo:         repo,
+		logger:       log,
+		tokenManager: tokenManager,
+		redisUser:    redisUser,
 	}
 }
 
@@ -45,10 +52,57 @@ func (u *UserService) CreateUser(ctx context.Context, name, surname, email, pass
 	return userID, nil
 }
 
+func (u *UserService) LoginUser(ctx context.Context, email, password string) (*domain.UserByEmail, error) {
+
+	userByEmail, err := u.repo.GetUserByEmail(ctx, email, password)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := checkPassword(password, userByEmail.Password); err != nil {
+		return nil, err
+	}
+
+}
+
 func generatePasswordHash(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	if err != nil {
 		return "", fmt.Errorf("failed to hash password due to error %w", err)
 	}
 	return string(hash), nil
+}
+
+func checkPassword(passwordLogin, passwordDB string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordLogin), []byte(passwordDB))
+	if err != nil {
+		return fmt.Errorf("password does not match")
+	}
+	return nil
+}
+
+func (s *UserService) createSession(ctx context.Context, userUUID string) (jwt.Tokens, error) {
+
+	var (
+		res jwt.Tokens
+		err error
+	)
+
+	res.AccessToken, err = s.tokenManager.NewJWT(userUUID)
+	if err != nil {
+		return res, err
+	}
+
+	res.RefreshToken, err = s.tokenManager.NewRefreshToken()
+	if err != nil {
+		return res, err
+	}
+	session := Session{
+		UserUUID:  userUUID,
+		ExpiresAt: time.Duration(s.refreshTokenTTL) * time.Minute,
+	}
+
+	err = s.redis.SetSession(ctx, res.RefreshToken, session)
+
+	return res, err
 }
