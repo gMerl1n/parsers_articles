72f61c97cc693212c22d579a9a149f62b1dbe79b
Vladislav <talcore@yandex.ru>
2025-01-13 21:36:48 +0500

    fix: entity data_to_parse

--- 01_searcher_articles/internal/entities/data_to_parse.go
+++ 01_searcher_articles/internal/entities/data_to_parse.go
@@ -1,17 +1,19 @@
 package entities
 
 type DataForParsing struct {
+	IDCategory  int
 	UrlCategory string
 	Provider    string
 	Articles    []Article
 	Deep        int64
 }
 
-func NewDataForParsing(urlCategory, provider string, deep int64) *DataForParsing {
+func NewDataForParsing(urlCategory, provider string, deep int64, IDCategory int) *DataForParsing {
 
 	sliceArticles := make([]Article, 0)
 
 	return &DataForParsing{
+		IDCategory:  IDCategory,
 		UrlCategory: urlCategory,
 		Provider:    provider,
 		Articles:    sliceArticles,
